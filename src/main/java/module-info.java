396d0ca98651833f4fe208ec67f7ede0715b768c
Aptech Student <student@aahn02.edu>
2024-07-15 20:14:48 +0700

    add login

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.hung.do_an {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires java.base;
     opens com.hung.do_an to javafx.fxml;
     exports com.hung.do_an;
 }
