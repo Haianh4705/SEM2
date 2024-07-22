d7dd3dbec6798568789da7932b888be37d5db0d7
ZjjnN02 <dgbach2702@gmail.com>
2024-07-22 19:09:53 +0700

    up

--- src/main/java/com/hung/do_an/App.java
+++ src/main/java/com/hung/do_an/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("login"), 600, 350);
+        scene = new Scene(loadFXML("login"), 800, 600);
         stage.setScene(scene);
         stage.show();
     }
