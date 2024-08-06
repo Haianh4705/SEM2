b977d0e74b32cbae7a797e79c5e65acaae271d27
Haianh4705 <haianhnguyen472005@gmail.com>
2024-08-06 10:10:52 +0700

    update db

--- Untitled.sql
+++ Untitled.sql
@@ -26,26 +26,17 @@ CREATE TABLE `admin` (
 
 CREATE TABLE `classes` (
   `id` varchar(255) PRIMARY KEY,
-  `name` varchar(255)
-);
-
-CREATE TABLE `class_manager` (
-  `class_id` varchar(255),
+  `name` varchar(255),
   `teacher_id` varchar(255),
   `subject_id` varchar(255),
-  `time_start` time,
-  `time_end` time
+  `time_start` timestamp,
+  `time_end` timestamp
 );
 
 CREATE TABLE `subjects` (
   `id` varchar(255) PRIMARY KEY,
   `name` varchar(255),
   `semester` varchar(255)
-);
-
-CREATE TABLE `teacher_subject_manager` (
-  `teacher_id` varchar(255),
-  `subject_id` varchar(255)
 );
 
 CREATE TABLE `classes_student` (
@@ -57,23 +48,15 @@ CREATE TABLE `classes_student` (
 
 CREATE TABLE `marks` (
   `student_id` varchar(255),
-  `subject_name` varchar(255),
-  `exam_name` varchar(255),
-  `exam_time` time,
+  `subject_id` varchar(255),
   `mark` float
 );
 
 ALTER TABLE `teachers` ADD FOREIGN KEY (`id`) REFERENCES `user` (`id`);
 
-ALTER TABLE `class_manager` ADD FOREIGN KEY (`teacher_id`) REFERENCES `teachers` (`id`);
+ALTER TABLE `classes` ADD FOREIGN KEY (`teacher_id`) REFERENCES `teachers` (`id`);
 
-ALTER TABLE `class_manager` ADD FOREIGN KEY (`class_id`) REFERENCES `classes` (`id`);
-
-ALTER TABLE `class_manager` ADD FOREIGN KEY (`subject_id`) REFERENCES `subjects` (`id`);
-
-ALTER TABLE `teacher_subject_manager` ADD FOREIGN KEY (`teacher_id`) REFERENCES `teachers` (`id`);
-
-ALTER TABLE `teacher_subject_manager` ADD FOREIGN KEY (`subject_id`) REFERENCES `subjects` (`id`);
+ALTER TABLE `classes` ADD FOREIGN KEY (`subject_id`) REFERENCES `subjects` (`id`);
 
 ALTER TABLE `marks` ADD FOREIGN KEY (`student_id`) REFERENCES `students` (`id`);
 
@@ -86,3 +69,5 @@ ALTER TABLE `classes_student` ADD FOREIGN KEY (`student_id`) REFERENCES `student
 ALTER TABLE `classes_student` ADD FOREIGN KEY (`class_id`) REFERENCES `classes` (`id`);
 
 ALTER TABLE `students` ADD FOREIGN KEY (`id`) REFERENCES `user` (`id`);
+
+ALTER TABLE `marks` ADD FOREIGN KEY (`subject_id`) REFERENCES `subjects` (`id`);
